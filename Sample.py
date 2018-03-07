6d55e426c21523f5d9c33ec0072d512619875983
SagiAmar84 <sagi@beyondverbal.com>
2018-03-07 13:43:27 +0200

    Update Sample.py

--- Sample.py
+++ Sample.py
@@ -5,7 +5,11 @@ def getAnalysis(API_Key,WavPath):
     res = requests.post("https://token.beyondverbal.com/token",data={"grant_type":"client_credentials","apiKey":API_Key})
     token = res.json()['access_token']
     headers={"Authorization":"Bearer "+token}
-    pp = requests.post("https://apiv4.beyondverbal.com/v4/recording/start",json={"dataFormat": { "type":"WAV" }},verify=False,headers=headers)
+    #Please change the URL to the desired API version
+    # https://apiv3.beyondverbal.com/v3/recording/start
+    # https://apiv4.beyondverbal.com/v4/recording/start
+    # https://apiv5.beyondverbal.com/v5/recording/start
+    pp = requests.post("https://apiv4.beyondverbal.com/v5/recording/start",json={"dataFormat": { "type":"WAV" }},verify=False,headers=headers)
     if pp.status_code != 200:
         print(pp.status_code, pp.content)
         return
